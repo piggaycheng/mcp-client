6ad4fb19271d68eebf26555fa0a802b4faffe44d
Yu Cheng <yucheng@mirle.com.tw>
2025-04-16 17:42:32 +0800

    fix: use llama-index

--- src/mcp_client/main.py
+++ src/mcp_client/main.py
@@ -1,95 +1,71 @@
 import os
 from typing import Optional, Literal
-from contextlib import AsyncExitStack
 
 from dotenv import load_dotenv
-from ollama import AsyncClient
-from fastmcp import Client
-from fastmcp.client.transports import StdioTransport
+
+from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
+from llama_index.core.agent.workflow import FunctionAgent, ReActAgent
+from llama_index.llms.ollama import Ollama
 
 
 # Load environment variables from .env file
 load_dotenv()
 DEMO_MCP_SERVER_URL = os.getenv("DEMO_MCP_SERVER_URL")
-DEMO_MCP_SERVER_PYTHON_PATH = os.getenv("DEMO_MCP_SERVER_PYTHON_PATH")
-DEMO_MCP_SERVER_SCRIPT_PATH = os.getenv("DEMO_MCP_SERVER_SCRIPT_PATH")
+GITHUB_PERSONAL_ACCESS_TOKEN = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
+OLLAMA_HOST = os.getenv("OLLAMA_HOST")
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
+
+
+llm = Ollama(model=OLLAMA_MODEL, base_url=OLLAMA_HOST, request_timeout=60)
 
 
 class MCPClient():
     def __init__(self):
-        self.exit_stack = AsyncExitStack()
-        self.mcp_client: Optional[Client] = None
+        self.mcp_client: Optional[BasicMCPClient] = None
 
     async def connect_to_server(self, transport_type=Literal['stdio', 'sse']):
         """Connect to an MCP server"""
 
         if transport_type == 'sse':
-            self.mcp_client = await self.exit_stack.enter_async_context(Client(DEMO_MCP_SERVER_URL))
+            self.mcp_client = BasicMCPClient(DEMO_MCP_SERVER_URL)
+            self.mcp_tool_spec = McpToolSpec(client=self.mcp_client)
         elif transport_type == 'stdio':
-            transport = StdioTransport(
-                command=DEMO_MCP_SERVER_PYTHON_PATH,
-                args=[DEMO_MCP_SERVER_SCRIPT_PATH],
+            self.mcp_client = BasicMCPClient(
+                command_or_url='wsl',
+                args=[
+                    '-u',
+                    'root',
+                    'docker',
+                    'run',
+                    '--rm',
+                    '-i',
+                    '-e',
+                    'GITHUB_PERSONAL_ACCESS_TOKEN',
+                    'mcp/github',
+                ],
+                env={
+                    'GITHUB_PERSONAL_ACCESS_TOKEN': GITHUB_PERSONAL_ACCESS_TOKEN,
+                }
             )
-            self.mcp_client = await self.exit_stack.enter_async_context(Client(transport=transport))
+            self.mcp_tool_spec = McpToolSpec(client=self.mcp_client)
 
         response = await self.mcp_client.list_tools()
-        tools = response
-        print("Connected to server with tools:", [tool.name for tool in tools])
+        print("Tools: ", [tool.name for tool in response.tools])
 
     async def process_query(self, query: str) -> str:
         """Process a query using Ollama and available tools"""
-        response = await self.mcp_client.list_tools()
-        available_tools = [{
-            "name": tool.name,
-            "description": tool.description,
-            "input_schema": tool.inputSchema
-        } for tool in response]
-        print("Available tools:", available_tools)
-        messages = [
-            {
-                "role": "system",
-                "content": """
-                    You are an assistant that should decide which tool to use based on the query and the available tools.
-                    Should use the following JSON schema to format the response:
-                    {{
-                        "properties": {{
-                            "tool": {{
-                                "description": "Tool to use",
-                                "type": "string"
-                            }},
-                            "arguments": {{
-                                "description": "Function arguments",
-                                "type": "object"
-                            }}
-                        }}
-                    }}
-                    Available tools: {available_tools}
-                    If no tool is suitable, tool should be empty string and parameters should be empty.
-                """.format(available_tools=str(available_tools))
-            },
-            {
-                "role": "user",
-                "content": query
-            }
-        ]
-        response = await AsyncClient(
-            host=os.getenv("OLLAMA_HOST"),
-        ).chat(
-            model=os.getenv("OLLAMA_MODEL"),
-            messages=messages,
-            # tools=available_tools,
-        )
-        print("Response from Ollama:", response.message.content)
 
-    async def cleanup(self):
-        """Clean up resources"""
-        await self.exit_stack.aclose()
+        tools = await self.mcp_tool_spec.to_tool_list_async()
+        agent = ReActAgent(
+            name="Agent",
+            llm=llm,
+            tools=tools,
+        )
+        response = await agent.run(user_msg=query)
+        print("Response:", response)
 
 
 async def run():
     client = MCPClient()
-    try:
-        await client.connect_to_server(transport_type='stdio')
-        await client.process_query("what is the weather in Taitong?")
-    finally:
-        await client.cleanup()
+    await client.connect_to_server(transport_type='stdio')
+    await client.process_query("Use tool to search user 'piggaycheng' in github")

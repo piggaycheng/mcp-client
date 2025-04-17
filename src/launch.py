0b3f76a3adaa892d1fd69d31d24fcbadab3d46dd
Yu Cheng <yucheng@mirle.com.tw>
2025-04-17 10:58:16 +0800

    refactor: enhance MCPClient with FunctionCallingAgent and improve query processing

--- src/launch.py
+++ src/launch.py
@@ -1,6 +1,7 @@
 import asyncio
-from mcp_client.main import run
+from mcp_client.main import run, test
 
 
 if __name__ == '__main__':
     asyncio.run(run())
+    # asyncio.run(test())
--- src/mcp_client/main.py
+++ src/mcp_client/main.py
@@ -4,7 +4,10 @@ from typing import Optional, Literal
 from dotenv import load_dotenv
 
 from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
-from llama_index.core.agent.workflow import FunctionAgent, ReActAgent
+# from llama_index.core.agent.workflow import FunctionAgent, ReActAgent
+from llama_index.core.agent.react import ReActAgent
+from llama_index.core.agent.function_calling import FunctionCallingAgent
+from llama_index.core.tools import FunctionTool
 from llama_index.llms.ollama import Ollama
 
 
@@ -39,8 +42,6 @@ class MCPClient():
                     'run',
                     '--rm',
                     '-i',
-                    '-e',
-                    'GITHUB_PERSONAL_ACCESS_TOKEN',
                     'mcp/github',
                 ],
                 env={
@@ -50,22 +51,43 @@ class MCPClient():
             self.mcp_tool_spec = McpToolSpec(client=self.mcp_client)
 
         response = await self.mcp_client.list_tools()
-        print("Tools: ", [tool.name for tool in response.tools])
+        print("Tools: ", [tool.name for tool in response.tools], end="\n\n")
 
     async def process_query(self, query: str) -> str:
         """Process a query using Ollama and available tools"""
 
         tools = await self.mcp_tool_spec.to_tool_list_async()
-        agent = ReActAgent(
-            name="Agent",
+        agent = FunctionCallingAgent.from_tools(
             llm=llm,
             tools=tools,
+            verbose=True,
         )
-        response = await agent.run(user_msg=query)
+        response = await agent.achat(query)
         print("Response:", response)
 
 
 async def run():
     client = MCPClient()
     await client.connect_to_server(transport_type='stdio')
-    await client.process_query("Use tool to search user 'piggaycheng' in github")
+    await client.process_query("Search user in github")
+
+
+# -------------------------test----------------------------
+def multiply(a: float, b: float) -> float:
+    """Useful for multiplying two numbers."""
+    return a * b
+
+
+tools = [
+    FunctionTool.from_defaults(multiply),
+]
+# print(tools[0].metadata)
+
+
+async def test():
+    agent = ReActAgent.from_tools(
+        tools=tools,
+        llm=llm,
+        verbose=True,
+    )
+    print(await agent.achat("What is 1234 * 4567?"))
